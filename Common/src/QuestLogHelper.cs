99b8d9e943ec9de766850f56b452272965181723
desto-git <desto-git@users.noreply.github.com>
2020-10-10 15:41:48 +0200

    [GD] handle receiving quest items at a later date

---
+++ Common/src/QuestLogHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace Common
+{
+	using System.Collections.Generic;
+	using StardewValley;
+	using StardewValley.Quests;
+
+	/// <summary>Utility functions to work with the various quest types.</summary>
+	internal static class QuestLogHelper
+	{
+		/// <summary>Get a daily quest the player has accepted.</summary>
+		/// <returns>Array of daily quests.</returns>
+		public static ItemDeliveryQuest[] GetDailyItemDeliveryQuests()
+		{
+			List<ItemDeliveryQuest> quests = new List<ItemDeliveryQuest>();
+
+			var enumerator = Game1.player.questLog.GetEnumerator();
+			while (enumerator.MoveNext())
+			{
+				// Mailbox quests have an ID greater than 0
+				if (enumerator.Current.id.Value == 0 && enumerator.Current is ItemDeliveryQuest itemDeliveryQuest)
+				{
+					quests.Add(itemDeliveryQuest);
+				}
+			}
+
+			return quests.ToArray();
+		}
+	}
+}
